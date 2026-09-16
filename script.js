6f8ca6c09473f292608b8547e1dfd7d8db1a2b45
alchimiste0 <alchimiste0.gaming.room@gmail.com>
2026-09-17 00:14:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,12 +23,14 @@ if (!sequences) {
             {name: "SPONSORS_2", isVideo: false}, 
             {name: "VIDEO_PUB_Maison-de-la-literie", isVideo: true}, 
             {name: "VIDEO_PUB_Story", isVideo: true}
+            {name: "VIDEO_PUB_LCdB", isVideo: true}
         ],
         finPubs: [
             {name: "SPONSORS", isVideo: false}, 
             {name: "SPONSORS_2", isVideo: false}, 
             {name: "VIDEO_PUB_Maison-de-la-literie", isVideo: true}, 
             {name: "VIDEO_PUB_Story", isVideo: true}
+            {name: "VIDEO_PUB_LCdB", isVideo: true}
         ],
         entreeJoueurs: []
     };
